7e597df9f8a727a1c625a8597938a03035a0d490
Miguel Ángel Tribaldos <mtribaldos@gmail.com>
2015-07-31 10:00:55 +0200

    Polished to make a more pythonic version of the program

--- minimal-secret-santa.py
+++ minimal-secret-santa.py
@@ -7,36 +7,73 @@ from email.mime.text import MIMEText
 import yaml
 import sys
 
-config = yaml.load(open(sys.argv[1], 'r'))
-peerlist = config['participants']
-template_msg = config['message']
 
-sequence = range(len(peerlist.keys()))
-randsequence = sequence[:]
-
-eqcount = 1
-while eqcount > 0:
-        eqcount = 0;
-        random.shuffle(randsequence)
-        for x in sequence:
-                if x == randsequence[x]:
-                        eqcount = eqcount + 1
-
-s = smtplib.SMTP(config['mail_smtp_server'], config['mail_smtp_port'])
-s.ehlo()
-s.starttls()
-s.ehlo()
-s.login(config['mail_smtp_login'], config['mail_smtp_passwd'])
+def reshuffled_range(num_items):
+    """
+    Generate a reshuffled n-items range 
+    """
+    shuffled_range = range(num_items)
+    random.shuffle(shuffled_range)
+    return shuffled_range
 
 
-for x in sequence:
-        msgpeer = peerlist.keys()[x]
-        dstpeer = peerlist.keys()[randsequence[x]]
-        msg = MIMEText(template_msg.replace('%msgpeer%', msgpeer).replace('%dstpeer%', dstpeer).encode('utf-8'), "html")
-        you = peerlist[msgpeer]
-        msg['Subject'] = config['subject'].replace('%msgpeer%', msgpeer).replace('%dstpeer%', dstpeer).encode('utf-8')
+def check_all_shuffled(sequence):
+    """
+    Check there is any element in the original position
+    """
+    return not any([ index == value for (index, value) in enumerate(sequence) ])
 
-        msg['From'] = config['mail_from']
-        msg['To'] = you
-        s.sendmail("", [you], msg.as_string())
-        print you, msg.as_string()
+
+def reshuffled_tested_range(num_items):
+    """
+    Shuffle a list of n items without any coincidence with its previous state    
+    """
+    while True:
+        sequence = reshuffled_range(num_items)
+        if check_all_shuffled(sequence):
+            break    
+    return sequence
+
+
+def templatized_message(msg):
+    """
+    Prepare the message substituing some defined variables with its value
+    """
+    return msg.replace('%src_peer%', msg_peer).replace('%dst_peer%', dst_peer).encode('utf-8')
+
+
+def send_mail(from_index, to_index):
+    """
+    Generate and send the Secret Santa message 
+    """
+    src_peer = peer_list.keys()[from_index]
+    dst_peer = peer_list.keys()[to_index]
+    message = MIMEText(templatized_msg(config['message']), "html")
+    mail_address = peerlist[src_peer]
+    msg['Subject'] = templatized_msg(config['subject'])
+    msg['From'] = config['mail_from']
+    msg['To'] = mail_address
+    s.sendmail("", [mail_address], message.as_string())
+
+
+def init_mail_system():
+    """
+    Prepare the mail system
+    """
+    s = smtplib.SMTP(config['mail_smtp_server'], config['mail_smtp_port'])
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login(config['mail_smtp_login'], config['mail_smtp_passwd'])
+
+
+
+if __name__ == '__main__':
+    config = yaml.load(open(sys.argv[1], 'r'))
+    peer_list = config['participants']
+    init_mail_system()
+
+    reshuffled_sequence = reshuffled_tested_range(len(peer_list.keys()))
+    [ send_mail(src, dst) for (src, dst) in enumerate(reshuffled_sequence) ]
+
+# vim: set expandtab ts=4 sw=4:
